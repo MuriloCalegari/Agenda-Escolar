47b8fcbf878d685d651262315a1211ffb5af55aa
Murilo Calegari <murilo.calegari.souza@gmail.com>
2019-05-04 22:43:48 -0300

    Add copyright to AboutPage

--- app/src/main/java/calegari/murilo/agendaescolar/AboutActivity.java
+++ app/src/main/java/calegari/murilo/agendaescolar/AboutActivity.java
@@ -19,54 +19,82 @@ import org.w3c.dom.Attr;
 
 public class AboutActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        String appDescription = getString(R.string.app_about_1) + "\n \n" + getString(R.string.app_about_2);
+		String appDescription = getString(R.string.app_about_1) + "\n \n" + getString(R.string.app_about_2);
 
-        Element openSourceElement = new Element();
-        openSourceElement.setTitle(getString(R.string.open_source_libraries));
-        openSourceElement.setIconDrawable(R.drawable.ic_code_black_24dp);
-        openSourceElement.setOnClickListener(v -> openLibrariesDialog());
+		Element openSourceElement = new Element();
+		openSourceElement.setTitle(getString(R.string.open_source_libraries));
+		openSourceElement.setIconDrawable(R.drawable.ic_code_black_24dp);
+		openSourceElement.setOnClickListener(v -> openLibrariesDialog());
 
-        View aboutPage = new AboutPage(this)
-                .isRTL(false)
-                .setDescription(appDescription)
-                .setImage(R.mipmap.ic_launcher)
-                .addItem(new Element().setTitle(getString(R.string.code_version) + " " + BuildConfig.VERSION_NAME))
-                .addPlayStore(getApplicationContext().getPackageName())
-                .addEmail("murilo.calegari.souza@gmail.com")
-                .addGitHub("MuriloCalegari/Agenda-Escolar")
-                .addItem(openSourceElement)
-                .create();
+		Element copyrightElement = new Element();
+		copyrightElement
+				.setTitle(getString(R.string.copyright))
+				.setIconDrawable(R.drawable.ic_copyright_black_24dp)
+				.setOnClickListener(v -> openCopyrightDialog());
 
-        setContentView(R.layout.toolbar);
+		View aboutPage = new AboutPage(this)
+				.isRTL(false)
+				.setDescription(appDescription)
+				.setImage(R.mipmap.ic_launcher)
+				.addItem(new Element().setTitle(getString(R.string.code_version) + " " + BuildConfig.VERSION_NAME))
+				.addPlayStore(getApplicationContext().getPackageName())
+				.addEmail("murilo.calegari.souza@gmail.com")
+				.addGitHub("MuriloCalegari/Agenda-Escolar")
+				.addItem(openSourceElement)
+				.addItem(copyrightElement)
+				.create();
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+		setContentView(R.layout.toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
-        // Finishes the Activity
-        toolbar.setNavigationOnClickListener(v -> finish());
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        LinearLayout mActivityRoot = (findViewById(R.id.main_toolbar)); // our host view
-        mActivityRoot.addView(aboutPage, 1); // Add about-page view to position 1 (since 0 is already taken by the toolbar)
-    }
+		// Finishes the Activity
+		toolbar.setNavigationOnClickListener(v -> finish());
 
-    private void openLibrariesDialog() {
+		LinearLayout mActivityRoot = (findViewById(R.id.main_toolbar)); // our host view
+		mActivityRoot.addView(aboutPage, 1); // Add about-page view to position 1 (since 0 is already taken by the toolbar)
+	}
 
-        Attribution VERTICAL_STEPPER_FORM = ((new Attribution.Builder("Vertical Stepper Form")
-		        .addCopyrightNotice("Copyright 2018 Julio Ernesto Cabañas")
-		        .addLicense(License.APACHE)
-		        .setWebsite("https://github.com/ernestoyaquello/VerticalStepperForm"))
-		        .build());
-        Attribution MATERIAL_SEEK_BAR_PREFERENCE = ((new Attribution.Builder("Material SeekBarPreference")
-		        .addCopyrightNotice("Copyright 2016 Pavel Sikun")
-		        .addLicense(License.APACHE)
-		        .setWebsite("https://github.com/MrBIMC/MaterialSeekBarPreference"))
-		        .build());
+	private void openCopyrightDialog() {
+		Attribution SCHOOL_VECTOR_FREEPIK = ((new Attribution.Builder("School vector")
+				.addCopyrightNotice("Created by freepik - wwww.freepik.com")
+				.setWebsite("https://www.freepik.com/free-photos-vectors/school"))
+				.build());
+
+		Attribution BACKGROUND_VECTOR_FREEPIK = ((new Attribution.Builder("Background Vector")
+				.addCopyrightNotice("Created by pikisuperstar - www.freepik.com")
+				.setWebsite("https://www.freepik.com/free-photos-vectors/background"))
+				.build());
+
+		AttributionPresenter attributionPresenter = new AttributionPresenter.Builder(this)
+				.addAttributions(
+						SCHOOL_VECTOR_FREEPIK,
+						BACKGROUND_VECTOR_FREEPIK
+				)
+				.build();
+
+		attributionPresenter.showDialog(getString(R.string.copyright));
+	}
+
+	private void openLibrariesDialog() {
+
+		Attribution VERTICAL_STEPPER_FORM = ((new Attribution.Builder("Vertical Stepper Form")
+				.addCopyrightNotice("Copyright 2018 Julio Ernesto Cabañas")
+				.addLicense(License.APACHE)
+				.setWebsite("https://github.com/ernestoyaquello/VerticalStepperForm"))
+				.build());
+		Attribution MATERIAL_SEEK_BAR_PREFERENCE = ((new Attribution.Builder("Material SeekBarPreference")
+				.addCopyrightNotice("Copyright 2016 Pavel Sikun")
+				.addLicense(License.APACHE)
+				.setWebsite("https://github.com/MrBIMC/MaterialSeekBarPreference"))
+				.build());
 		Attribution ANDROID_ABOUT_PAGE = (new Attribution.Builder("Android About Page")
 				.addCopyrightNotice("Copyright 2019 Mehdi Sakout")
 				.addLicense(License.MIT).setWebsite("https://github.com/medyo/android-about-page"))
@@ -89,22 +117,22 @@ public class AboutActivity extends AppCompatActivity {
 				.setWebsite("https://github.com/tobiasschuerg/android-week-view"))
 				.build();
 
-        AttributionPresenter attributionPresenter = new AttributionPresenter.Builder(this)
-		        .addAttributions(new Attribution.Builder("AttributionPresenter")
-				        .addCopyrightNotice("Copyright 2017 Francisco José Montiel Navarro")
-				        .addLicense(License.APACHE)
-				        .setWebsite("https://github.com/franmontiel/AttributionPresenter")
-				        .build())
-                .addAttributions(
-                        VERTICAL_STEPPER_FORM,
-                        MATERIAL_SEEK_BAR_PREFERENCE,
-		                ANDROID_ABOUT_PAGE,
-		                INBOX_RECYCLER_VIEW,
-		                SLIM_CHART,
-		                MPANDROIDCHART,
-		                ANDROID_WEEK_VIEW)
-                .build();
+		AttributionPresenter attributionPresenter = new AttributionPresenter.Builder(this)
+				.addAttributions(new Attribution.Builder("AttributionPresenter")
+						.addCopyrightNotice("Copyright 2017 Francisco José Montiel Navarro")
+						.addLicense(License.APACHE)
+						.setWebsite("https://github.com/franmontiel/AttributionPresenter")
+						.build())
+				.addAttributions(
+						VERTICAL_STEPPER_FORM,
+						MATERIAL_SEEK_BAR_PREFERENCE,
+						ANDROID_ABOUT_PAGE,
+						INBOX_RECYCLER_VIEW,
+						SLIM_CHART,
+						MPANDROIDCHART,
+						ANDROID_WEEK_VIEW)
+				.build();
 
-        attributionPresenter.showDialog(getString(R.string.open_source_libraries));
-    }
+		attributionPresenter.showDialog(getString(R.string.open_source_libraries));
+	}
 }
